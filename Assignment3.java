6454bcfdbc8eaea92f8605a061dd17fc10e2822c
Madhuash <rao.madhavi57@gmail.com>
2016-03-15 16:25:07 +0530

    Added files via upload

---
+++ Assignment3.java
@@ -0,0 +1,13 @@
+
+public class Assignment3 {
+	public static void main(String args[])
+	{
+	try
+	{
+		int i = (Integer) null;
+		System.out.println("int value = " + i);
+		
+	} catch(NullPointerException  e) {System.out.println("NullPointerException  " + e); } 
+	
+	}
+}
